27a79afc50a90011f6dab3d469e0a4c5e0b89d62
Dima Provorov <dprovorov@agilevision.io>
2019-08-02 14:20:12 +0300

    SRX-3240: Update Pricing tests

--- resources/py/sftp.py
+++ resources/py/sftp.py
@@ -1,27 +1,35 @@
 import paramiko
+import os
 
 def sftpConnect(my_username,my_key_filename,my_host='sftp.storeroomlogix.com',my_port=22):
+    os.umask(0)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(my_host, port=my_port, username=my_username, key_filename=my_key_filename, timeout=20)
+    os.umask(0)
     sftp = ssh.open_sftp()
     return sftp
 
 def sftpPutFile(sftp,localpath,remotepath):
+    os.umask(0)
     file = sftp.put(localpath,remotepath)
     return file
 
 def sftpClose(sftp):
+    os.umask(0)
     sftp.close()
 
 def sftpGetFile(sftp,remotepath,localpath):
+    os.umask(0)
     file = sftp.get(remotepath,localpath)
     return file
 
 def sftpListFolder(sftp,path):
+    os.umask(0)
     list = sftp.listdir(path)
     return list
 
 def sftpCompareLists(first_list,second_list):
+    os.umask(0)
     result = list(set(second_list) - set(first_list))
     return result
