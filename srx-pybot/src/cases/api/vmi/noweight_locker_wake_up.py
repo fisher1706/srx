ab63edb403403e50a2b3262bfbb75b774ada994c
Dima Provorov <dprovorov@agilevision.io>
2020-02-06 15:29:22 +0200

    SRX-7024: Update testrail case id

--- srx-pybot/src/cases/api/vmi/noweight_locker_wake_up.py
+++ srx-pybot/src/cases/api/vmi/noweight_locker_wake_up.py
@@ -13,7 +13,7 @@ import copy
 
 def noweight_locker_wake_up(case):
     case.log_name("NoWeight locker Wake Up")
-    case.testrail_config(case.activity.variables.run_number, 1887)
+    case.testrail_config(case.activity.variables.run_number, 1888)
 
     try:
         timestamp_another_day = 1736035200000
