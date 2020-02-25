1f6d28b19373ec4e5aa5283dbeff864f70b5b9f7
Dima Provorov <dprovorov@agilevision.io>
2020-02-25 11:06:25 +0200

    Remove unused wait

--- srx-pybot/src/waits/not_first_page_bootstrap.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-class not_first_page_bootstrap(object):
-    def __call__(self, driver):
-        if (driver.find_element_by_xpath("//li[@class='active page-item']").get_attribute("title") == "1"):
-            return False
-        else:
-            return True
