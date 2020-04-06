3a52968eabdabf1076197d33df113bf83b60a522
Veronika Shara <vshara@agilevision.io>
2020-04-06 17:37:00 +0300

    SRX-8197: Updated gitignore and fixed import name

--- srx-pybot/src/waits/is_progress_bar_loading.py
+++ srx-pybot/src/waits/is_progress_bar_loading.py
@@ -4,7 +4,6 @@ class is_progress_bar_loading(object):
     def __call__(self, driver):
         element = driver.find_element_by_xpath("//div[@role='progressbar']")
         count = len(element.get_attribute("class"))
-        print(count)
         if count == 23:
             return True
         if count == 29:
