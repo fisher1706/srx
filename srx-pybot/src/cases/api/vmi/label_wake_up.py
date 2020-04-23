d7cc9d3fa2a60a6c4176c5d1a41f7a602cb144c3
Dima Provorov <dprovorov@agilevision.io>
2020-04-23 12:03:04 +0300

    Update label wake up test

--- srx-pybot/src/cases/api/vmi/label_wake_up.py
+++ srx-pybot/src/cases/api/vmi/label_wake_up.py
@@ -6,6 +6,7 @@ from src.api.admin.admin_billing_api import AdminBillingApi
 from src.api.distributor.transaction_api import TransactionApi
 from src.api.distributor.settings_api import SettingsApi
 from src.bases.location_basis import location_basis
+import time
 
 def label_wake_up(case):
     case.log_name("Label Wake Up")
@@ -26,20 +27,14 @@ def label_wake_up(case):
         sta.set_checkout_software_settings_for_shipto(new_shipto)
 
         aba.billing_transit(timestamp_another_day)
-        aba.billing_transit(timestamp_another_day)
 
         location_body = la.get_location_by_sku(new_shipto, product_body["partSku"])
         ordering_config_id = location_body[0]["orderingConfig"]["id"]
         assert location_body[0]["inventoryStatus"] == "FROZEN", f"Location should be in FROZEN inventory status, now {location_body[0]['inventoryStatus']}"
 
         ta.create_active_item(new_shipto, ordering_config_id)
-        location_body = la.get_location_by_sku(new_shipto, product_body["partSku"])
-        assert location_body[0]["inventoryStatus"] == "SLOW", f"Location should be in SLOW inventory status, now {location_body[0]['inventoryStatus']}"
-
         transaction_id = ta.get_transaction_id(sku=product_body["partSku"], shipto_id=new_shipto)
         ta.update_replenishment_item(transaction_id, product_body["roundBuy"], "DELIVERED")
-
-        ta.create_active_item(new_shipto, ordering_config_id)
         location_body = la.get_location_by_sku(new_shipto, product_body["partSku"])
         assert location_body[0]["inventoryStatus"] == "MOVING", f"Location should be in MOVING inventory status, now {location_body[0]['inventoryStatus']}"
 
