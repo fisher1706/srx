f783501421a343957a0d40062d41cac0a3f74425
dprovorov <dprovorov@agilevision.io>
2020-08-10 15:04:42 +0300

    Add some tests for serial numbers

--- src/cases/test_serialization_sn.py
+++ src/cases/test_serialization_sn.py
@@ -1,5 +1,6 @@
 import pytest
 import copy
+import time
 from src.resources.tools import Tools
 from src.api.setups.setup_location import SetupLocation
 from src.api.distributor.location_api import LocationApi
@@ -11,7 +12,6 @@ class TestSerializationSN():
         api.testrail_case_id = 2105
 
         response_location = SetupLocation(api).setup()
-
         sna = SerialNumberApi(api)
 
         sna.create_serial_number(response_location["location_id"], response_location["shipto_id"], Tools.random_string_u(), expected_status_code=400)
@@ -27,7 +27,6 @@ class TestSerializationSN():
         response_location_2 = setup_location.setup()
 
         sna = SerialNumberApi(api)
-
         sn = Tools.random_string_u()
 
         sna.create_serial_number(response_location_1["location_id"], response_location_1["shipto_id"], sn)
@@ -42,9 +41,7 @@ class TestSerializationSN():
 
         sna = SerialNumberApi(api)
         la = LocationApi(api)
-
         sn = Tools.random_string_u()
-
         sn_id = sna.create_serial_number(response_location["location_id"], response_location["shipto_id"], sn)
 
         locations = la.get_locations(response_location["shipto_id"])
@@ -59,3 +56,137 @@ class TestSerializationSN():
 
         locations = la.get_locations(response_location["shipto_id"])
         assert locations[0]["onHandInventory"] == 1
+
+    def test_ohi_serialized_product_location_equal_to_sn_in_available_status(self, api, delete_shipto):
+        api.testrail_case_id = 2109
+
+        setup_location = SetupLocation(api)
+        setup_location.setup_product.add_option("serialized")
+        response_location = setup_location.setup()
+
+        sna = SerialNumberApi(api)
+        la = LocationApi(api)
+        sn = Tools.random_string_u()
+        sn_id = sna.create_serial_number(response_location["location_id"], response_location["shipto_id"], sn)
+
+        locations = la.get_locations(response_location["shipto_id"])
+        assert locations[0]["onHandInventory"] == 0, "Serialized location should be created with OHI = 0"
+
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        sn_dto["status"] = "AVAILABLE"
+        sna.update_serial_number(sn_dto)
+
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        assert sn_dto["status"] == "AVAILABLE"
+
+        locations = la.get_locations(response_location["shipto_id"])
+        assert locations[0]["onHandInventory"] == 1
+
+    def test_sn_created_in_assigned_status_for_serialized_location(self, api, delete_shipto):
+        api.testrail_case_id = 2108
+
+        setup_location = SetupLocation(api)
+        setup_location.add_option("serialized")
+        response_location = setup_location.setup()
+
+        sna = SerialNumberApi(api)
+        sna.create_serial_number(response_location["location_id"], response_location["shipto_id"], Tools.random_string_u())
+
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        assert sn_dto["status"] == "ASSIGNED"
+
+    def test_sn_created_in_assigned_status_for_serialized_product_location(self, api, delete_shipto):
+        api.testrail_case_id = 2110
+
+        setup_location = SetupLocation(api)
+        setup_location.setup_product.add_option("serialized")
+        response_location = setup_location.setup()
+
+        sna = SerialNumberApi(api)
+        sna.create_serial_number(response_location["location_id"], response_location["shipto_id"], Tools.random_string_u())
+
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        assert sn_dto["status"] == "ASSIGNED"
+
+    def test_serialized_ohi_decreased_when_available_in_issued(self, api, delete_shipto):
+        api.testrail_case_id = 2111
+
+        setup_location = SetupLocation(api)
+        setup_location.add_option("serialized")
+        response_location = setup_location.setup()
+
+        sna = SerialNumberApi(api)
+        la = LocationApi(api)
+        sn = Tools.random_string_u()
+        sn_id = sna.create_serial_number(response_location["location_id"], response_location["shipto_id"], sn)
+
+        locations = la.get_locations(response_location["shipto_id"])
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        sn_dto["status"] = "AVAILABLE"
+        sna.update_serial_number(sn_dto)
+        locations = la.get_locations(response_location["shipto_id"])
+        assert locations[0]["onHandInventory"] == 1
+
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        sn_dto["status"] = "ISSUED"
+        sna.update_serial_number(sn_dto)
+
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        assert sn_dto["status"] == "ISSUED"
+
+        locations = la.get_locations(response_location["shipto_id"])
+        assert locations[0]["onHandInventory"] == 0
+
+    def test_serialized_ohi_decreased_when_available_in_expired(self, api, delete_shipto):
+        api.testrail_case_id = 2112
+
+        setup_location = SetupLocation(api)
+        setup_location.add_option("serialized")
+        response_location = setup_location.setup()
+
+        sna = SerialNumberApi(api)
+        la = LocationApi(api)
+        sn = Tools.random_string_u()
+        sn_id = sna.create_serial_number(response_location["location_id"], response_location["shipto_id"], sn)
+
+        locations = la.get_locations(response_location["shipto_id"])
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        sn_dto["status"] = "AVAILABLE"
+        sna.update_serial_number(sn_dto)
+        locations = la.get_locations(response_location["shipto_id"])
+        assert locations[0]["onHandInventory"] == 1
+
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        sn_dto["dateExpiration"] = time.time()*1000
+        sna.update_serial_number(sn_dto)
+
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        assert sn_dto["status"] == "EXPIRED"
+
+        locations = la.get_locations(response_location["shipto_id"])
+        assert locations[0]["onHandInventory"] == 0
+
+    def test_cannot_update_sn_available_to_assigned(self, api, delete_shipto):
+        api.testrail_case_id = 2113
+
+        setup_location = SetupLocation(api)
+        setup_location.add_option("serialized")
+        response_location = setup_location.setup()
+
+        sna = SerialNumberApi(api)
+        la = LocationApi(api)
+        sn = Tools.random_string_u()
+        sn_id = sna.create_serial_number(response_location["location_id"], response_location["shipto_id"], sn)
+
+        locations = la.get_locations(response_location["shipto_id"])
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        sn_dto["status"] = "AVAILABLE"
+        sna.update_serial_number(sn_dto)
+
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        assert sn_dto["status"] == "AVAILABLE"
+        sn_dto["status"] = "ASSIGNED"
+        sna.update_serial_number(sn_dto)
+
+        sn_dto = sna.get_serial_number(shipto_id=response_location["shipto_id"])[0]
+        assert sn_dto["status"] == "AVAILABLE"
