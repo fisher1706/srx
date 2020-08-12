6950feb71b508798df366b30b1b4e1db18c49a0a
dprovorov <dprovorov@agilevision.io>
2020-08-12 16:35:55 +0300

    Implement tests for serial numbers

--- src/api/distributor/location_api.py
+++ src/api/distributor/location_api.py
@@ -59,3 +59,13 @@ class LocationApi(API):
             self.logger.info("Location was successfully deleted")
         else:
             self.logger.error(str(response.content))
+
+    @Decorator.default_expected_code(200)
+    def location_bulk_update(self, action, shipto_id, expected_status_code, all=False, customer_id=None, ids=None):
+        if (customer_id is None):
+            customer_id = self.data.customer_id
+        url = self.url.get_api_url_for_env(f"/distributor-portal/distributor/customers/{customer_id}/shiptos/{shipto_id}/locations/bulkUpdate?action={action}&all={all}")
+        token = self.get_distributor_token()
+        response = self.send_post(url, token, ids)
+        assert expected_status_code == response.status_code, f"Incorrect status_code! Expected: '{expected_status_code}'; Actual: {response.status_code}; Repsonse content:\n{str(response.content)}"
+        self.logger.info(f"Location bulk update ended with status_code = '{response.status_code}', as expected: {response.content}")
