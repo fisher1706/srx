075e6d5a1dec45547fff072a24d9ea5584b2e0c6
Dmytro Beloplotov <dbeloplotov@agilevision.io>
2020-09-23 11:32:44 +0300

    implement method for qnty alligned to round buy

--- src/mobile_cases/test_scan_to_order.py
+++ src/mobile_cases/test_scan_to_order.py
@@ -1,7 +1,6 @@
 from src.api.mobile.mobile_transaction_api import MobileTransactionApi
 from src.api.distributor.transaction_api import TransactionApi
 from src.api.setups.setup_location import SetupLocation
-from src.api.setups.setup_product import SetupProduct
 import pytest
 
 
@@ -160,9 +159,24 @@ class TestScanToOrder():
         
         assert len(transactions_after_re_request) == len(transactions), "Transactions already exist in Active status"
 
+
+    @pytest.mark.parametrize("conditions", [
+        {
+            "status": "ORDERED",
+            "testrail_case_id": 2190
+        },
+        {
+            "status": "SHIPPED",
+            "testrail_case_id": 2192
+        },
+        {
+            "status": "QUOTED",
+            "testrail_case_id": 2193
+        }
+        ])
     @pytest.mark.regression
-    def test_bulk_create_for_exist_transactions_in_ordered(self, mobile_api, delete_shipto):
-        mobile_api.testrail_case_id = 2190
+    def test_bulk_create_for_exist_transactions_in_different_status(self, mobile_api, conditions, delete_shipto):
+        mobile_api.testrail_case_id = conditions["testrail_case_id"]
         mta = MobileTransactionApi(mobile_api)
         ta = TransactionApi(mobile_api)
 
@@ -187,12 +201,12 @@ class TestScanToOrder():
 
         transactions_in_active = ta.get_transaction(shipto_id=response_location_1["shipto_id"],status="ACTIVE")["entities"]
 
-        ta.update_replenishment_item(transactions_in_active[0]["id"],response_location_1['product']['roundBuy']*3,"ORDERED")
-        ta.update_replenishment_item(transactions_in_active[1]["id"],response_location_2['product']['roundBuy']*3,"ORDERED")
+        ta.update_replenishment_item(transactions_in_active[0]["id"],response_location_1['product']['roundBuy']*3,conditions["status"])
+        ta.update_replenishment_item(transactions_in_active[1]["id"],response_location_2['product']['roundBuy']*3,conditions["status"])
 
         transactions_after_update = ta.get_transaction(shipto_id=response_location_1["shipto_id"])["entities"]
 
-        assert transactions_after_update[0]["status"] == "ORDERED" and transactions_after_update[1]["status"] == "ORDERED", "Status of each transaction should be ORDERED"
+        assert transactions_after_update[0]["status"] == conditions["status"] and transactions_after_update[1]["status"] == conditions["status"], "Status of each transaction should be equal condition status"
         
         mta.bulk_create(response_location_1["shipto_id"], data)
         transactions_in_active = ta.get_transaction(shipto_id=response_location_1["shipto_id"],status="ACTIVE")["entities"]
@@ -202,15 +216,22 @@ class TestScanToOrder():
         transactions = ta.get_transaction(shipto_id=response_location_1["shipto_id"])["entities"]
 
         assert len(transactions) == 4 , "There are should be 4 transactions in the given ShipTo "
-    
+
+    @pytest.mark.parametrize("conditions", [
+        {
+            "quantity": 11,
+            "testrail_case_id": 2187
+        }
+        ])
     @pytest.mark.regression
-    def test_bulk_create_for_exist_transactions_in_shipped(self, mobile_api, delete_shipto):
-        mobile_api.testrail_case_id = 2192
+    def test_bulk_create_with_qnty_not_alligned_roundBuy(self, mobile_api, conditions):
+        mobile_api.testrail_case_id = 2187
         mta = MobileTransactionApi(mobile_api)
         ta = TransactionApi(mobile_api)
 
         setup_location = SetupLocation(mobile_api)
         setup_location.setup_shipto.add_option("checkout_settings", "DEFAULT")
+        setup_location.setup_product.add_option("round_buy", 5)
         response_location_1 = setup_location.setup()
         setup_location.add_option("shipto_id", response_location_1["shipto_id"])
         response_location_2 = setup_location.setup()
@@ -218,87 +239,12 @@ class TestScanToOrder():
         data = [
             {
                 "partSku": response_location_1["product"]["partSku"],
-                "quantity": response_location_1["product"]["roundBuy"] * 3
+                "quantity": conditions["quantity"]
             },
             {
                 "partSku": response_location_2["product"]["partSku"],
-                "quantity": response_location_2["product"]["roundBuy"] * 3
+                "quantity": conditions["quantity"]
             }
         ]
 
         mta.bulk_create(response_location_1["shipto_id"], data)
-
-        transactions_in_active = ta.get_transaction(shipto_id=response_location_1["shipto_id"],status="ACTIVE")["entities"]
-
-        ta.update_replenishment_item(transactions_in_active[0]["id"],response_location_1['product']['roundBuy']*3,"SHIPPED")
-        ta.update_replenishment_item(transactions_in_active[1]["id"],response_location_2['product']['roundBuy']*3,"SHIPPED")
-
-        transactions_after_update = ta.get_transaction(shipto_id=response_location_1["shipto_id"])["entities"]
-
-        assert transactions_after_update[0]["status"] == "SHIPPED" and transactions_after_update[1]["status"] == "SHIPPED", "Status of each transaction should be SHIPPED"
-        
-        mta.bulk_create(response_location_1["shipto_id"], data)
-        transactions_in_active = ta.get_transaction(shipto_id=response_location_1["shipto_id"],status="ACTIVE")["entities"]
-
-        assert transactions_in_active[0]["reorderQuantity"] == 0 and transactions_in_active[1]["reorderQuantity"] == 0, "Reorder quantity of each transactions should be equal to 0"
-
-        transactions = ta.get_transaction(shipto_id=response_location_1["shipto_id"])["entities"]
-
-        assert len(transactions) == 4 , "There are should be 4 transactions in the given ShipTo "
-
-    @pytest.mark.regression
-    def test_bulk_create_for_exist_transactions_in_quoted(self, mobile_api, delete_shipto):
-        mobile_api.testrail_case_id = 2193
-        mta = MobileTransactionApi(mobile_api)
-        ta = TransactionApi(mobile_api)
-
-        setup_location = SetupLocation(mobile_api)
-        setup_location.setup_shipto.add_option("checkout_settings", "DEFAULT")
-        response_location_1 = setup_location.setup()
-        setup_location.add_option("shipto_id", response_location_1["shipto_id"])
-        response_location_2 = setup_location.setup()
-
-        data = [
-            {
-                "partSku": response_location_1["product"]["partSku"],
-                "quantity": response_location_1["product"]["roundBuy"] * 3
-            },
-            {
-                "partSku": response_location_2["product"]["partSku"],
-                "quantity": response_location_2["product"]["roundBuy"] * 3
-            }
-        ]
-
-        mta.bulk_create(response_location_1["shipto_id"], data)
-
-        transactions_in_active = ta.get_transaction(shipto_id=response_location_1["shipto_id"],status="ACTIVE")["entities"]
-
-        ta.update_replenishment_item(transactions_in_active[0]["id"],response_location_1['product']['roundBuy']*3,"QUOTED")
-        ta.update_replenishment_item(transactions_in_active[1]["id"],response_location_2['product']['roundBuy']*3,"QUOTED")
-
-        transactions_after_update = ta.get_transaction(shipto_id=response_location_1["shipto_id"])["entities"]
-
-        assert transactions_after_update[0]["status"] == "QUOTED" and transactions_after_update[1]["status"] == "QUOTED", "Status of each transaction should be QUOTED"
-        
-        mta.bulk_create(response_location_1["shipto_id"], data)
-        transactions_in_active = ta.get_transaction(shipto_id=response_location_1["shipto_id"],status="ACTIVE")["entities"]
-
-        assert transactions_in_active[0]["reorderQuantity"] == 0 and transactions_in_active[1]["reorderQuantity"] == 0, "Reorder quantity of each transactions should be equal to 0"
-
-        transactions = ta.get_transaction(shipto_id=response_location_1["shipto_id"])["entities"]
-
-        assert len(transactions) == 4 , "There are should be 4 transactions in the given ShipTo "
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
