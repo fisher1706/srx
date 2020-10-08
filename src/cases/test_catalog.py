369ffbdd3d5cf022581bebb06dbdc2a90877dc9b
dprovorov <dprovorov@agilevision.io>
2020-10-08 13:04:56 +0300

    Update product import template customer security group CRUD test

--- src/cases/test_catalog.py
+++ src/cases/test_catalog.py
@@ -96,7 +96,7 @@ class TestCatalog():
         product_body["roundBuy"] = "39"
         #-------------------
         products = [
-            [product_body["partSku"], None, None, product_body["shortDescription"], None, None, None, None, None, None, None, None, None, product_body["roundBuy"], None, None, None, None, None, None, None, None, None, None, None, None, None]
+            [product_body["partSku"], None, None, product_body["shortDescription"], None, None, None, None, None, None, None, None, None, product_body["roundBuy"], None, None, None, None, None, None, None, None, None, None, None, None, None, None]
         ]
         #-------------------
 
@@ -213,7 +213,7 @@ class TestCatalog():
         product_body["roundBuy"] = "39"
         #-------------------
         products = [
-            [product_body["partSku"], None, None, product_body["shortDescription"], None, None, None, None, None, None, None, None, None, product_body["roundBuy"], None, None, None, None, None, None, None, None, None, None, None, None, None]
+            [product_body["partSku"], None, None, product_body["shortDescription"], None, None, None, None, None, None, None, None, None, product_body["roundBuy"], None, None, None, None, None, None, None, None, None, None, None, None, None, None]
         ]
         #-------------------
 
