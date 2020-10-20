54dc7050e89c9a3b3e73b5fa835a66b4c51a9070
dprovorov <dprovorov@agilevision.io>
2020-10-20 13:10:39 +0300

    Update default SG for qa env

--- src/resources/data.py
+++ src/resources/data.py
@@ -65,7 +65,7 @@ class Data():
         self.customer_user_passcode = "PSCD"
         self.customer_user_first_name = "dprovorov+cust@agilevision.io"
         self.customer_user_last_name = "dprovorov+cust@agilevision.io"
-        self.default_security_group_id = 15
+        self.default_security_group_id = 2160
 
 class SmokeData():
     def __init__(self, environment):
