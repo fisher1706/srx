2bbd7c04de3bade15c40d9341fe58375daeadf91
dprovorov <dprovorov@agilevision.io>
2020-11-26 10:17:29 +0200

    Test false assert

--- src/cases/test_authorization.py
+++ src/cases/test_authorization.py
@@ -154,6 +154,7 @@ class BaseAuthorization():
         lp.input_email(context.customer_email)
         lp.input_password(context.customer_password)
         lp.click_on_submit_button()
+        assert False
         cpp.click_id(Locator.id_enter_here, timeout=30)
         cpp.customer_sidebar_should_contain_email()
         cpp.url_should_contain("customer")
