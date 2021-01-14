144c9e1aa678f408353cdd0267b3c913a15f89e2
dprovorov <dprovorov@agilevision.io>
2021-01-14 13:43:18 +0200

    Implement test for updating min and max on cp

--- src/api/customer/customer_vmi_list_api.py
+++ src/api/customer/customer_vmi_list_api.py
@@ -1,4 +1,6 @@
 from src.api.api import API
+from src.fixtures.decorators import Decorator
+from src.resources.messages import Message
 
 class CustomerVmiListApi(API):
     def get_locations(self, distributor_id=None, shipto_id=None):
@@ -18,3 +20,14 @@ class CustomerVmiListApi(API):
             self.logger.error(str(response.content))
         response_json = response.json()
         return response_json["data"]["entities"]
+
+    @Decorator.default_expected_code(200)
+    def update_location(self, dto, expected_status_code):
+        url = self.url.get_api_url_for_env(f"/customer-portal/customer/locations/update")
+        token = self.get_customer_token()
+        response = self.send_post(url, token, dto)
+        assert expected_status_code == response.status_code, Message.assert_status_code.format(expected_status_code=expected_status_code, actual_status_code=response.status_code, content=response.content)
+        if (response.status_code == 200):
+            self.logger.info(f"Location with SKU = '{dto[0]['orderingConfig']['product']['partSku']}' has been successfully updated")
+        else:
+            self.logger.info(Message.info_operation_with_expected_code.format(entity="Location", operation="updating", status_code=response.status_code, content=response.content))
