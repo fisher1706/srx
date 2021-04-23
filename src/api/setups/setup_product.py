55f90948b25b5c7fc079b7dae806eaeb067f43da
dprovorov <dprovorov@agilevision.io>
2021-04-23 09:48:46 +0300

    Implement test for reorder control with quantity on reorder

--- src/api/setups/setup_product.py
+++ src/api/setups/setup_product.py
@@ -48,5 +48,6 @@ class SetupProduct(BaseSetup):
             self.product = self.options["product"]
 
         self.id = pa.create_product(copy.deepcopy(self.product), expected_status_code=self.expected_status_code)
-        self.product["packageConversion"] = pa.get_product(self.product["partSku"])[0]["calculatedPackageConversion"]
+        if (self.expected_status_code is None):
+            self.product["packageConversion"] = pa.get_product(self.product["partSku"])[0]["calculatedPackageConversion"]
         self.product["id"] = self.id
