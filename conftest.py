e1d463c16e4eea47ff53c8f693aa626e8e283062
dprovorov <dprovorov@agilevision.io>
2021-10-04 17:46:18 +0300

    Update pytest_runtest_makereport method

--- conftest.py
+++ conftest.py
@@ -102,8 +102,13 @@ def driver(request, session_context):
     yield browser
     browser.quit()
 
-@pytest.mark.tryfirst
-def pytest_runtest_makereport(item, call, __multicall__):
-    rep = __multicall__.execute()
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
     setattr(item, "rep_" + rep.when, rep)
-    return rep
