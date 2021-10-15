4fa9a323f580abec70ac3b73967d6d726d63dd7f
dprovorov <dprovorov@agilevision.io>
2021-10-15 14:42:58 +0300

    Refactor serialization_sn tests with preset

--- src/fixtures/presets.py
+++ src/fixtures/presets.py
@@ -4,8 +4,8 @@ from src.api.setups.setup_location import SetupLocation
 
 @pytest.fixture(scope="function")
 def customer_organization_location_preset():
-    def wrapper(api, ohi="MAX", reorder_controls_settings="DEFAULT"):
-        setup_customer_location = SetupCustomerLocation(api)
+    def wrapper(context, ohi="MAX", reorder_controls_settings="DEFAULT"):
+        setup_customer_location = SetupCustomerLocation(context)
         setup_customer_location.add_option("ohi", ohi)
         setup_customer_location.setup_organization.add_option("site")
         setup_customer_location.setup_organization.add_option("subsite")
@@ -17,11 +17,11 @@ def customer_organization_location_preset():
 
 @pytest.fixture(scope="function")
 def serialized_location_preset():
-    def wrapper(api, round_buy=1, serialization_settings=None):
-        setup_location = SetupLocation(api)
+    def wrapper(context, round_buy=1, serialization_settings=None, lot=None):
+        setup_location = SetupLocation(context)
         setup_location.add_option("serialized")
+        setup_location.add_option("lot", lot)
         setup_location.setup_product.add_option("round_buy", round_buy)
-        if serialization_settings is not None:
-            setup_location.setup_shipto.add_option("serialization_settings", serialization_settings)
+        setup_location.setup_shipto.add_option("serialization_settings", serialization_settings)
         return setup_location.setup()
     return wrapper
