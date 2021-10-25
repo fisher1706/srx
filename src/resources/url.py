91cff27282917de3dc2ae3cb16cf7887a3e758ec
Dmytro Provorov <dprovorov@agilevision.io>
2021-10-25 15:52:43 +0300

    Refactor code

--- src/resources/url.py
+++ src/resources/url.py
@@ -3,7 +3,7 @@ class URL():
         self.environment = environment
         if self.environment is None:
             self.environment = 'qa'
-        if (self.environment == 'dev' or self.environment == 'staging' or self.environment == 'prod' or self.environment == 'qa'):
+        if self.environment in ('dev', 'staging', 'prod', 'qa'):
             self.admin_portal = self.get_url_for_env("storeroomlogix.com", "admin")
             self.auth_portal = self.get_url_for_env("storeroomlogix.com", "auth")
             self.distributor_portal = self.get_url_for_env("storeroomlogix.com", "distributor")
