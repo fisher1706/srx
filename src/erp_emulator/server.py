51bd4854369d1930052676ae813176566b789d5c
Dmytro Provorov <dprovorov@agilevision.io>
2021-11-20 12:14:43 +0200

    Add root endpoint to the ilx mocks

--- src/erp_emulator/server.py
+++ src/erp_emulator/server.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask import request
 import logging
 
+app = Flask(__name__)
+import ilx_base_mocks
+
 logging.basicConfig(filename='flask.log',level=logging.DEBUG)
 logger = logging.getLogger('flask_server')
-
-app = Flask(__name__)
 
 @app.before_request
 def log_request_info():
     logger.info(request.get_data())
 
-app.run(port='80', debug=True, host="0.0.0.0")
+app.run(port='8080', debug=True, host="0.0.0.0")
